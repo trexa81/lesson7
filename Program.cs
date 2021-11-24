fecffb745e01a79e8e15b066de07206b45be85c4
trexa81 <trex81@yandex.ru>
2021-11-24 22:21:34 +0300

    app.config

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using lesson7.Properties;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+//using System.Configuration;
 
 namespace lesson7
 {
@@ -6,9 +13,26 @@ namespace lesson7
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine(Resources.hello);
+            ResourceManager rm = new ResourceManager("lesson7.DataStrings",
+                Assembly.GetExecutingAssembly());
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+
+            //Console.WriteLine(rm.GetString("hello"));
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            int a = 7;
+            var b = 3;
             Console.WriteLine("Введите ваше имя");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Hello " + name);
+            string nameOne = Console.ReadLine();
+            Console.WriteLine($"Hello " + nameOne + "\t" + (a * b));
+            Console.ReadKey();
         }
     }
 }
